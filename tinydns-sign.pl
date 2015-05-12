bbf053ec694a654f9de71eb84d6ff60bde919a02
peter <peter@78b66a8a-d1b3-11e1-b6a3-5959136b85f8>
2015-05-12 19:56:14 +0000

    Actually the previous commit fixed a bug wrt the signing of TXT and generic records
    git-svn-id: file:///home/peter/mnt/root/SVN/tinydnssec/trunk@35 78b66a8a-d1b3-11e1-b6a3-5959136b85f8

--- tinydns-sign.pl
+++ tinydns-sign.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-# (C) 2012,2015 Peter Conrad <conrad@quisquis.de>
+# Copyright (C) 2012,2015 Peter Conrad <conrad@quisquis.de>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3 as
